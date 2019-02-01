9867636622b8851dcca668795c741ae826590228
ASUS <hutasoitnoel@gmail.comgit config --global user.name noel>
2019-02-01 13:56:54 +0700

    Edited exercise-4.js

--- exercise-4.js
+++ exercise-4.js
@@ -1,183 +1,59 @@
+// Menggunakan Switch-Case
 var tanggal = 29;
 var bulan = 2;
-var tahun = 2001;
+var tahun = 2024;
 
-switch(bulan){
-  case 1:
-    switch(true){
-      case 1<=tanggal:
-        switch(true){
-          case tanggal<=31:
-            switch (true){
-              case 1900<=tahun:
-                switch(true){
-                  case tahun<=2200: console.log(tanggal + " Januari " + tahun)
-                }
-            }
-        }
-    } break;
-  case 2:
-    switch(true){
-      case 1<=tanggal:
-        switch(true){
-          case tanggal<=28:
-            switch (true){
-              case 1900<=tahun:
-                switch(true){
-                  case tahun<=2200: console.log(tanggal + " Februari " + tahun)
-                }
-            }
-        }
-        switch(tanggal){
-          case 29:
-            switch(true){
-              case 1900<=tahun:
-                switch(true){
-                  case tahun<=2200:
-                    switch(tahun%4){
-                      case 0:
-                        switch(true){
-                          case tahun%100>0:
-                            console.log(tanggal + " Februari " + tahun);
-                          case tahun%100===0:
-                            switch (tahun%400){
-                              case 0: 
-                                console.log(tanggal + " Februari " + tahun)
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    } break;
-  case 3:
-    switch(true){
-      case 1<=tanggal:
-        switch(true){
-          case tanggal<=31:
-            switch (true){
-              case 1900<=tahun:
-                switch(true){
-                  case tahun<=2200: console.log(tanggal + " Maret " + tahun)
-                }
-            }
-        }
-    } break;
-case 4:
-    switch(true){
-      case 1<=tanggal:
-        switch(true){
-          case tanggal<=30:
-            switch (true){
-              case 1900<=tahun:
-                switch(true){
-                  case tahun<=2200: console.log(tanggal + " April " + tahun)
-                }
-            }
-        }
-    } break;
-case 5:
-    switch(true){
-      case 1<=tanggal:
-        switch(true){
-          case tanggal<=31:
-            switch (true){
-              case 1900<=tahun:
-                switch(true){
-                  case tahun<=2200: console.log(tanggal + " Mei " + tahun)
-                }
-            }
-        }
-    } break;
-case 6:
-    switch(true){
-      case 1<=tanggal:
-        switch(true){
-          case tanggal<=30:
-            switch (true){
-              case 1900<=tahun:
-                switch(true){
-                  case tahun<=2200: console.log(tanggal + " Juni " + tahun)
-                }
-            }
-        }
-    } break;
-case 7:
-    switch(true){
-      case 1<=tanggal:
-        switch(true){
-          case tanggal<=31:
-            switch (true){
-              case 1900<=tahun:
-                switch(true){
-                  case tahun<=2200: console.log(tanggal + " Juli " + tahun)
-                }
-            }
-        }
-    } break;
-case 8:
-    switch(true){
-      case 1<=tanggal:
-        switch(true){
-          case tanggal<=31:
-            switch (true){
-              case 1900<=tahun:
-                switch(true){
-                  case tahun<=2200: console.log(tanggal + " Agustus " + tahun)
-                }
-            }
-        }
-    } break;
-case 9:
-    switch(true){
-      case 1<=tanggal:
-        switch(true){
-          case tanggal<=30:
-            switch (true){
-              case 1900<=tahun:
-                switch(true){
-                  case tahun<=2200: console.log(tanggal + " September " + tahun)
-                }
-            }
-        }
-    } break;
-case 10:
-    switch(true){
-      case 1<=tanggal:
-        switch(true){
-          case tanggal<=31:
-            switch (true){
-              case 1900<=tahun:
-                switch(true){
-                  case tahun<=2200: console.log(tanggal + " Oktober " + tahun)
-                }
-            }
-        }
-    } break;
-case 11:
-    switch(true){
-      case 1<=tanggal:
-        switch(true){
-          case tanggal<=30:
-            switch (true){
-              case 1900<=tahun:
-                switch(true){
-                  case tahun<=2200: console.log(tanggal + " November " + tahun)
-                }
-            }
-        }
-    } break;
-case 12:
-    switch(true){
-      case 1<=tanggal:
-        switch(true){
-          case tanggal<=31:
-            switch (true){
-              case 1900<=tahun:
-                switch(true){
-                  case tahun<=2200: console.log(tanggal + " Desember " + tahun)
-                }
-            }
-        }
-    } break;
+switch (bulan){
+  case 1: if (1<=tanggal && tanggal<=31 && 1900<=tahun && tahun<=2200) {
+            console.log(tanggal + " Januari " + tahun);
+          } 
+          break;
+  case 2: if (1 <= tanggal && tanggal <= 28 && 1900 <= tahun && tahun <= 2200) {
+            console.log(tanggal + " Februari " + tahun);
+          } else if (tanggal === 29 && 1900 <= tahun && tahun<=2200 && tahun % 4 === 0 && tahun%100 !== 0) {
+            console.log(tanggal + " Februari " + tahun)
+          } else if (tanggal === 29 && 1900<=tahun && tahun<=2200 && tahun%4 === 0 && tahun%100 === 0 && tahun%400 === 0){
+            console.log(tanggal + " Februari " + tahun)
+          } 
+          break;
+  case 3: if (1<=tanggal && tanggal<=31 && 1900<=tahun && tahun<=2200){
+            console.log(tanggal + " Maret " + tahun);
+          } 
+          break;
+  case 4: if (1<=tanggal && tanggal<=30 && 1900<=tahun && tanggal<=2200){
+            console.log(tanggal + " April " + tahun);
+          } 
+          break;
+  case 5: if (1<=tanggal && tanggal<=31 && 1900<=tahun && tahun<=2200){
+            console.log(tanggal + " Mei " + tahun);
+          } 
+          break;
+  case 6: if (1<=tanggal && tanggal<=30 && 1900<=tahun && tanggal<=2200){
+            console.log(tanggal + " Juni " + tahun);
+          } 
+          break;
+  case 7: if (1<=tanggal && tanggal<=31 && 1900<=tahun && tahun<=2200){
+            console.log(tanggal + " Juli " + tahun);
+          } 
+          break;
+  case 8: if (1<=tanggal && tanggal<=31 && 1900<=tahun && tahun<=2200){
+            console.log(tanggal + " Agustus " + tahun);
+          } 
+          break;
+  case 9: if (1<=tanggal && tanggal<=30 && 1900<=tahun && tanggal<=2200){
+            console.log(tanggal + " September " + tahun);
+          } 
+          break;
+  case 10: if (1<=tanggal && tanggal<=31 && 1900<=tahun && tahun<=2200){
+            console.log(tanggal + " Oktober " + tahun);
+          } 
+          break;
+  case 11: if (1<=tanggal && tanggal<=30 && 1900<=tahun && tanggal<=2200){
+            console.log(tanggal + " November " + tahun);
+          } 
+          break;
+  case 12: if (1<=tanggal && tanggal<=31 && 1900<=tahun && tahun<=2200){
+            console.log(tanggal + " Desember " + tahun);
+          }  
+          break;
 }
